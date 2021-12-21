42f217ac288960c1478e2223f22159fd80d8c1b4
Hugo Gruson <hugo.gruson@protonmail.com>
2021-12-21 09:13:42 +0100

    Specify full output name

--- main.sh
+++ main.sh
@@ -6,4 +6,4 @@ else
   LAST_SUNDAY=$(date --date='last Sun' +'%Y-%m-%d')
 fi
 mkdir -p data-processed/UpgUmibUsi-MultiBayes
-wget -P data-processed/UpgUmibUsi-MultiBayes https://www.dropbox.com/s/m9pxh715g0bvofs/$LAST_SUNDAY-UpgUmibUsi-MultiBayes.csv?dl=0
+wget -O data-processed/UpgUmibUsi-MultiBayes/$LAST_SUNDAY-UpgUmibUsi-MultiBayes.csv https://www.dropbox.com/s/m9pxh715g0bvofs/$LAST_SUNDAY-UpgUmibUsi-MultiBayes.csv?dl=0
